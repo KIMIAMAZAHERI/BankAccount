db7b19d40f6be4846139970e9d1b2bdd2352aa17
Kimia Mazaheri <98044191+KIMIAMAZAHERI@users.noreply.github.com>
2022-05-27 21:47:22 +0200

    the problems solved

--- script/script.js
+++ script/script.js
@@ -1,73 +1,78 @@
  
- function atm() {
+ 
+let account = {
+  
+    accountName :"test",
+     balance : 100,
 
-    var choice = parseInt(prompt("Please press 1 for deposite 2 for withdraw and 3 for see the balance of your account."));
-   this.atm =choice;
-   console.log(this.atm);
-   switch (choice){
-       case "1":
-           this.deposit();
-        case"2":
-           this.withdrawal();
-        case"3":
-           this.getBlance();
-        default:
+     atm:function(){
+
+      var choice = parseInt(prompt("Please press 1 for deposite 2 for withdraw."));
+     this.atm =choice;
+    
+     if (choice===1){
+        this.deposit();
+        console.loggingout;
+        
+     }
+     else if (choice===2){
+      this.withdrawal();
+      console.loggingout;
+     }
+     else{
         console.log("Please select one of these choices")
         this.atm();
-   }
-   console.log(this);
-};
-atm();
-console.log(atm);
-
-let account = {
-   accountName :this.accountName,
-   balance : this.balance,
-
+     }
+   },
+   
     getBlance:function( amount = 100 ){
-        // balance = (this.deposit- this.withdrawal);
-        this.balance = amount;
-        console.log (this.balance);
+       
+       alert (" Hi  " +  this.accountName + " the balance of your account is " + this.balance);
     },
     deposit:function(){
-       var depo=parseFloat(prompt("Please inter amount of your deposit"));
+       const depo=parseFloat(prompt("Please inter amount of your deposit"));
        this.deposit=depo;
        this.balance = (this.balance + this.deposit);
       if (depo >= 0.00){
-        console.log("YaY, you have " +  this.deposit  + " SEK deposit on your account! ");
+        alert ("YaY, you have " +  this.deposit  + " SEK deposit on your account! ");
        }else{ 
-       console.log("Sorry, wrong input!");
+       alert ("Sorry, wrong input!");
       }
       console.log(this);
    }, 
     withdrawal:function(){
-       var withdraw=parseFloat(prompt("Please inter amount of your withdrawal "));
+       const withdraw=parseFloat(prompt("Please inter amount of your withdrawal "));
        this.withdrawal= withdraw;
        this.balance = (this.balance - this.withdrawal);
      if (withdraw >= 0){
-         console.log ("Hi, "+this.accountName  +" You wants withdraw " + this.withdrawal +" SEK from your account! ");
+         alert (" You wants withdraw " + this.withdrawal +" SEK from your account! ");
       }else{ 
-           console.log ("Sorry, wrong input!");
+           alert ("Sorry, wrong input!");
        }
        console.log(this);
     },
-   getAccountName:function(){ 
-        var person = prompt("Please inter your name?");
-        this.accountName = person;
-        console.log(this);
-      }, 
-   accountError:function(){
-    if (this.withdrawal >= this.balance){
-        console.log ("You can not withdraw more than 100 SEK, Please try again! ");
-        this.account();
-    }s
-  }
+    getAccountName:function(){ 
+      const person = prompt("Please inter your name?");
+      this.accountName = person;
+      alert ("Hi " + this.accountName);
+    }, 
+     
+
+   //accountError:function(){
+   // if (this.withdrawal >= this.balance){
+    //    console.log ("You can not withdraw more than 100 SEK, Please try again! ");
+      //  this.account();
+   // }s;
+ // }
 };
-account.getBlance();
+account.atm();
 account.getAccountName();
+account.getBlance();
 account.deposit();
 account.withdrawal();
-console.log("Hi " + account.accountName +" the balance of your account is " + account.balance + " SEK");
+
+
+
 
 
 
